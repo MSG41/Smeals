1f0591e9e5cafd65ea038577d3a139a5ec30a181
Salimitless <s.thaifa@amsterdam.nl>
2023-06-22 23:19:02 +0200

    Search fix - refined search

--- src/components/Search/Search.vue
+++ src/components/Search/Search.vue
@@ -1,42 +1,27 @@
+<!-- Search.vue -->
 <template>
   <div class="search-container">
     <input class="search-input" v-model="searchQuery" placeholder="Search meals..." />
-    <div class="meals" v-if="hasResults">
-      <MealCard v-for="meal in filteredMeals" :key="meal.idMeal" :meal="meal" />
-    </div>
     <div v-if="isLoading" class="loading">Loading...</div>
-    <div v-if="!hasResults && !isLoading" class="no-results">No results found.</div>
   </div>
 </template>
 
 <script lang="ts">
 import { ref, watch, computed } from 'vue'
-import MealCard from '@/components/MealCard/MealCard.vue'
 import { useHomeStore } from '@/stores/store'
 
 export default {
-  components: {
-    MealCard
-  },
-
   setup() {
     const searchQuery = ref('')
-
     const store = useHomeStore()
 
     watch(searchQuery, (newVal) => {
       store.searchMeals(newVal)
     })
 
-    const filteredMeals = computed(() => store.meals)
-
-    const hasResults = computed(() => filteredMeals.value.length > 0)
-
     return {
       searchQuery,
-      filteredMeals,
-      isLoading: computed(() => store.isLoading),
-      hasResults
+      isLoading: computed(() => store.isLoading)
     }
   }
 }
