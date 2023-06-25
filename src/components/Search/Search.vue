73aa77af0d9940f4194f88de2a29036e5ede65d7
Salimitless <s.thaifa@amsterdam.nl>
2023-06-26 01:00:44 +0200

    search query preserved. All filters totally functional.

--- src/components/Search/Search.vue
+++ src/components/Search/Search.vue
@@ -1,29 +1,34 @@
 <template>
-  <!-- Container for the search input -->
   <div class="search-container">
-    <!-- Search input field -->
     <input class="search-input" v-model="searchQuery" placeholder="Search meals..." />
   </div>
 </template>
 
 <script lang="ts">
-import { ref, watch, computed } from 'vue'
+import { ref, watch, computed, onMounted } from 'vue'
 import { useHomeStore } from '@/stores/store'
 
 export default {
   setup() {
-    // Reactive reference for the search query
-    const searchQuery = ref('')
-
-    // Access the home store
     const store = useHomeStore()
+    const searchQuery = ref(store.currentQuery)
 
-    // Watch for changes in the search query and trigger searchMeals in the store
     watch(searchQuery, (newVal) => {
       store.searchMeals(newVal)
     })
 
-    // Return the reactive reference for the search query and a computed property for the loading state
+    // update searchQuery when store's currentQuery changes
+    watch(
+      () => store.currentQuery,
+      (newVal) => {
+        searchQuery.value = newVal
+      }
+    )
+
+    onMounted(() => {
+      store.searchMeals(searchQuery.value)
+    })
+
     return {
       searchQuery,
       isLoading: computed(() => store.isLoading)
